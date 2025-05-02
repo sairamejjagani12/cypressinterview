cdbd49f3d32ec46e2b4503ed6531e7721fb118e2
sairamejjagani12 <ejjaganisairam12@gmail.com>
2025-05-02 16:45:04 +0530

    login is completed

---
+++ cypress/e2e/Pages/Login/Login.js
@@ -0,0 +1,58 @@
+class Login
+{
+    navigateToLoginPage(url) {
+        cy.visit(url);
+        cy.log("Succsfully navigated to Login Page");
+    }
+    validateLoginPage() {
+        cy.title().should("eq", "Swag Labs");
+        cy.log("Login page title validates");
+        cy.url().should("include", "https://www.saucedemo.com");
+        cy.log("Login url validated");
+    }
+    validateUsernameField() {
+        cy.get("#user-name").should('exist');
+    }
+    validatePasswordField() {
+        cy.get("#password").should("exist");
+    }
+    validateLoginButton() {
+        cy.get("#login-button").should("exist");
+    }
+    enterEmptyUsername(username) {
+        cy.get("#user-name").type(username);
+    }
+    enterEmptyPassword(password) {
+        cy.get("#password").type(password);
+    }
+    enterInvalidEmail(email) {
+        cy.get("#user-name").type(email);
+    }
+    enterInvalidPassword(password) {
+        cy.get("#password").type(password);
+    }
+    validateEmptyUsernameErrorMEssage(expected) {
+        cy.contains("Username is required").should("contain", expected);
+    }
+    validateEmptyPasswordErrorMEssage(expected) {
+        cy.contains("Password is required").should("contain", expected);
+    }
+    validateInvalidErrorMEssage(expected) {
+        cy.contains("Username and password do not match any user in this service").should("contain", expected);
+    }
+    enterUsername(username) {
+        cy.get("#user-name").type(username).should("have.value", username);
+    }
+    enterPassword(password) {
+        cy.get("#password").type(password).should("have.value", password);
+    }
+    clickLoginButton() {
+        cy.get("#login-button").click();
+    }
+    clickLogoutButton() {
+        cy.wait(3000)
+        cy.get("#react-burger-menu-btn").click();
+        cy.contains("Logout").click();
+    }
+}
+export default Login
---
+++ cypress/e2e/TestScripts/Login/Login.cy.js
@@ -0,0 +1,84 @@
+/// <reference types="cypress"/>
+import Login from "../../Pages/Login/Login";
+describe('Entity Login test suite', () => {
+    const login = new Login();
+    let url;
+    let emptyUsername;
+    let emptyUsernamePassword;
+    let emptyUsernameError;
+    let emptyPasswordUsername;
+    let emptyPassword;
+    let emptyPasswordError;
+    let invalidUsername;
+    let invalidPassword;
+    let validUsername;
+    let validPassword;
+    let errorMEssage;
+    before("before", () => {
+        cy.fixture("Credentials.json").then((data) => {
+            url = data[3].url
+            emptyUsername = data[0].username;
+            emptyUsernamePassword = data[0].password;
+            emptyUsernameError = data[0].message;
+            emptyPasswordUsername = data[1].username;
+            emptyPassword = data[1].password;
+            emptyPasswordError = data[1].message;
+            invalidUsername = data[2].username;
+            invalidPassword = data[2].password;
+            validUsername = data[3].username;
+            validPassword = data[3].password;
+            errorMEssage = data[2].message;
+            cy.then("test data", () => {
+                console.log(validUsername);
+                console.log(validPassword);
+            })
+        })
+    })
+    beforeEach("before each", () => {
+        login.navigateToLoginPage(url);
+    })
+    it("Verify if user is able to see  Login page.", () => {
+        login.navigateToLoginPage(url);
+        login.validateLoginPage();
+    })
+    it("Verify the UI elements in Login page", () => {
+        login.validateUsernameField();
+        login.validatePasswordField();
+        login.validateLoginButton();
+    })
+    it("Verify if Username  is mandatory.", () => {
+        login.enterEmptyUsername(emptyUsername+"{Enter}")
+        login.enterPassword(emptyUsernamePassword);
+        cy.wait(3000);
+        login.validateEmptyUsernameErrorMEssage(emptyUsernameError);
+    })
+    it("Verify if  Password  is mandatory.", () => {
+        login.enterUsername(emptyPasswordUsername);
+        login.enterEmptyPassword(emptyPassword + "{Enter}")
+        cy.wait(3000);
+        login.validateEmptyPasswordErrorMEssage(emptyPasswordError);
+    })
+    it("Verifyn the Login with invalid username and  password.", () => {
+        login.enterUsername(invalidUsername);
+        login.enterPassword(invalidPassword);
+        login.clickLoginButton();
+        login.validateInvalidErrorMEssage(errorMEssage);
+    })
+    it('Verify the Login with valid credentials.', () => {
+        login.validateUsernameField();
+        login.enterUsername(validUsername);
+        login.validatePasswordField();
+        login.enterPassword(validPassword);
+        login.validateLoginButton();
+        login.clickLoginButton();
+    })
+    it("Verify the Logout of user.", () => {
+        login.validateUsernameField();
+        login.enterUsername(validUsername);
+        login.validatePasswordField();
+        login.enterPassword(validPassword);
+        login.validateLoginButton();
+        login.clickLoginButton();
+        login.clickLogoutButton();
+    })
+})
